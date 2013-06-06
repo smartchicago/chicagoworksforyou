c8267cd44b0475eced38938f77381c3d22460de3
cgansen <cgansen@gmail.com>
2013-06-06 00:17:33 -0500

    properly unmarshal to Open311Request type

--- workers/fetch.go
+++ workers/fetch.go
@@ -11,9 +11,9 @@ import (
 const OPEN311_API_URI = "http://311api.cityofchicago.org/open311/v2/requests.json"
 
 type Open311Request struct {
-	service_request_id, status, service_name, service_code, agency_responsible, address string
-	lat, long                                                                           float32
-	requested_datetime, updated_datetime                                                string // FIXME: should these be proper time objects?
+	Lat, Long                                                                           float64
+	Service_request_id, Status, Service_name, Service_code, Agency_responsible, Address string
+	Requested_datetime, Updated_datetime                                                string // FIXME: should these be proper time objects?
 }
 
 func main() {
@@ -32,15 +32,15 @@ func fetchRequests() {
 		if err == nil {
 			log.Println("loaded response body.")
 			// parse into JSON
-			var requests []map[string]interface{}
+
+			var requests []Open311Request
 			err := json.Unmarshal(body, &requests)
 			if err != nil {
 				log.Fatal("error parsing JSON:", err)
 			}
-			
-			for _, req := range requests {
-				fmt.Println(req)
-			}
+
+			log.Printf("received %d requests from Open311", len(requests))
+			fmt.Printf("%+v", requests)
 		}
 	} else {
 		log.Fatalln("error fetching from Open311 endpoint", err)
