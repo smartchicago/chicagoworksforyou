0b407a213a40b328466ed5c84a0efc000887da99
cgansen <cgansen@gmail.com>
2013-06-10 14:50:08 -0500

    poll on a 30 second interval

--- workers/fetch.go
+++ workers/fetch.go
@@ -29,6 +29,82 @@ func main() {
 	}
 	defer db.Close()
 
+	last_run_at := time.Now()
+
+	for {
+		switch {
+		case time.Since(last_run_at) > (30 * time.Second):
+			poll_open311(db)
+			last_run_at = time.Now()
+		default:
+			log.Print("sleeping for 10 seconds")
+			time.Sleep(10 * time.Second)
+		}
+	}
+}
+
+func (req Open311Request) String() string {
+	// pretty print SR information
+	return fmt.Sprintf("%s: %s at %s %f,%f, last update %s", req.Service_request_id, req.Service_name, req.Address, req.Lat, req.Long, req.Updated_datetime)
+}
+
+func fetchRequests() (requests []Open311Request) {
+	db, err := sql.Open("postgres", "dbname=cwfy sslmode=disable")
+	if err != nil {
+		log.Fatal("Cannot open database connection", err)
+	}
+	defer db.Close()
+
+	// find the most recent SR that we know about in the database
+	rows, err := db.Query("SELECT MAX(updated_datetime) FROM service_requests;")
+	if err != nil {
+		log.Fatal("error finding most recent service request", err)
+	}
+
+	last_updated_at := time.Now()
+	for rows.Next() {
+		if err := rows.Scan(&last_updated_at); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("most recent SR timestamp %s", last_updated_at)
+	}
+
+	// janky hack to transform the last updated timestamp into
+	// a format that plays nicely with the Open311 API
+	// FIXME: there HAS to be a better way to handle this.
+	formatted_date_string := last_updated_at.Format(time.RFC3339)
+	formatted_date_string_with_tz := formatted_date_string[0:len(formatted_date_string)-1] + "-0500" // trunc the trailing 'Z' and tack on timezone
+
+	// construct the request URI using base params and the proper time
+	open311_api_endpoint := OPEN311_API_URI + "&updated_after=" + formatted_date_string_with_tz
+
+	log.Printf("fetching from %s", open311_api_endpoint)
+	resp, err := http.Get(open311_api_endpoint)
+	defer resp.Body.Close()
+
+	if err != nil {
+		log.Fatalln("error fetching from Open311 endpoint", err)
+	}
+
+	// load response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("error loading response body", err)
+	}
+
+	// parse JSON and load into an array of Open311Request objects
+	err = json.Unmarshal(body, &requests)
+	if err != nil {
+		log.Fatal("error parsing JSON:", err)
+	}
+
+	log.Printf("received %d requests from Open311", len(requests))
+
+	return requests
+}
+
+func poll_open311(db *sql.DB) {
 	// load requests from open311
 	requests := fetchRequests()
 
@@ -117,65 +193,4 @@ func main() {
 			log.Fatal("error closing transaction", err)
 		}
 	}
-}
-
-func (req Open311Request) String() string {
-	// pretty print SR information
-	return fmt.Sprintf("%s: %s at %s %f,%f, last update %s", req.Service_request_id, req.Service_name, req.Address, req.Lat, req.Long, req.Updated_datetime)
-}
-
-func fetchRequests() (requests []Open311Request) {
-	db, err := sql.Open("postgres", "dbname=cwfy sslmode=disable")
-	if err != nil {
-		log.Fatal("Cannot open database connection", err)
-	}
-	defer db.Close()
-
-	// find the most recent SR that we know about in the database
-	rows, err := db.Query("SELECT MAX(updated_datetime) FROM service_requests;")
-	if err != nil {
-		log.Fatal("error finding most recent service request", err)
-	}
-
-	last_updated_at := time.Now()
-	for rows.Next() {
-		if err := rows.Scan(&last_updated_at); err != nil {
-			log.Fatal(err)
-		}
-
-		log.Printf("most recent SR timestamp %s", last_updated_at)
-	}
-
-	// janky hack to transform the last updated timestamp into
-	// a format that plays nicely with the Open311 API
-	// FIXME: there HAS to be a better way to handle this.
-	formatted_date_string := last_updated_at.Format(time.RFC3339)
-	formatted_date_string_with_tz := formatted_date_string[0:len(formatted_date_string)-1] + "-0500" // trunc the trailing 'Z' and tack on timezone
-
-	// construct the request URI using base params and the proper time
-	open311_api_endpoint := OPEN311_API_URI + "&updated_after=" + formatted_date_string_with_tz
-
-	log.Printf("fetching from %s", open311_api_endpoint)
-	resp, err := http.Get(open311_api_endpoint)
-	defer resp.Body.Close()
-
-	if err != nil {
-		log.Fatalln("error fetching from Open311 endpoint", err)
-	}
-
-	// load response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("error loading response body", err)
-	}
-
-	// parse JSON and load into an array of Open311Request objects
-	err = json.Unmarshal(body, &requests)
-	if err != nil {
-		log.Fatal("error parsing JSON:", err)
-	}
-
-	log.Printf("received %d requests from Open311", len(requests))
-	
-	return requests
 }
