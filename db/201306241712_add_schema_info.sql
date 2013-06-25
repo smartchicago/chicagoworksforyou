11414c77d9326737cb85ca8ea04fdfea7c9242b5
cgansen <cgansen@gmail.com>
2013-06-24 21:58:11 -0500

    add a very basic migration utility

---
+++ db/201306241712_add_schema_info.sql
@@ -0,0 +1,8 @@
+CREATE TABLE schema_info (version varchar(12));
+
+INSERT INTO schema_info (version) VALUES ('201306061151');
+INSERT INTO schema_info (version) VALUES ('201306071651');
+INSERT INTO schema_info (version) VALUES ('201306071725');
+INSERT INTO schema_info (version) VALUES ('201306091221');
+INSERT INTO schema_info (version) VALUES ('201306161511');
+INSERT INTO schema_info (version) VALUES ('201306241712');
