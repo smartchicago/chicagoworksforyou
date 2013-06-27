c501109323236ced97a7c93ac0654f6e6de3d26c
Sandor Weisz <santheo@mac.com>
2013-06-27 09:12:28 -0500

    Abastracted script to draw Chicago map.

--- frontend/src/js/wards-landing.js
+++ frontend/src/js/wards-landing.js
@@ -1,12 +1,5 @@
 buildWardPaths();
-
-var map = L.map('map',{scrollWheelZoom: false}).setView([41.83, -87.81], 11);
-L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    key: '302C8A713FF3456987B21FAAE639A13B',
-    maxZoom: 18
-}).addTo(map);
-map.zoomControl.setPosition('bottomleft');
+drawChicagoMap();
 
 for (var path in wardPaths) {
     var wardNum = parseInt(path,10) + 1;
--- frontend/src/js/yesterday.js
+++ frontend/src/js/yesterday.js
@@ -1,12 +1,5 @@
 $(function () {
     buildWardPaths();
-
-    window.map = L.map('map',{scrollWheelZoom: false}).setView([41.83, -87.81], 11);
-    L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        key: '302C8A713FF3456987B21FAAE639A13B',
-        maxZoom: 18
-    }).addTo(map);
-    map.zoomControl.setPosition('bottomright');
+    drawChicagoMap();
     window.allWards = L.layerGroup();
 });
