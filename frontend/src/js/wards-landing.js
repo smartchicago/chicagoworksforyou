ea90194817a1285e2369cc853549fa59d564344c
Sandor Weisz <santheo@mac.com>
2013-06-27 09:12:57 -0500

    Moved wards-landing code to an on-ready block.

--- frontend/src/js/wards-landing.js
+++ frontend/src/js/wards-landing.js
@@ -1,16 +1,18 @@
-buildWardPaths();
-drawChicagoMap();
+$(function () {
+    buildWardPaths();
+    drawChicagoMap();
 
-for (var path in wardPaths) {
-    var wardNum = parseInt(path,10) + 1;
-    var poly = L.polygon(
-        wardPaths[path],
-        {
-            color: '#0873AD',
-            opacity: 1,
-            weight: 2,
-            fillOpacity: (((wardNum % 5) + 2) / 10)
-        }
-    ).addTo(map);
-    poly.bindPopup('<a href="/wards/' + wardNum + '/">Ward ' + wardNum + '</a>');
-}
+    for (var path in wardPaths) {
+        var wardNum = parseInt(path,10) + 1;
+        var poly = L.polygon(
+            wardPaths[path],
+            {
+                color: '#0873AD',
+                opacity: 1,
+                weight: 2,
+                fillOpacity: (((wardNum % 5) + 2) / 10)
+            }
+        ).addTo(map);
+        poly.bindPopup('<a href="/wards/' + wardNum + '/">Ward ' + wardNum + '</a>');
+    }
+});
