9967c833458a9a82eb627acdd09691dcac113c54
Sandor Weisz <santheo@mac.com>
2013-07-03 08:39:51 -0500

    Added missing file from last commit.

--- frontend/src/js/scripts.js
+++ frontend/src/js/scripts.js
@@ -5,63 +5,83 @@ window.currWeekEnd = moment().day(-1).startOf('day');
 window.dateFormat = 'YYYY-MM-DD';
 window.weekDuration = moment.duration(6,"days");
 
-window.serviceTypesJSON = {
-    "graffiti_removal": {
+window.serviceTypesJSON = [
+    {
+        "slug": "graffiti_removal",
         "code": "4fd3b167e750846744000005",
         "name": "Graffiti Removal"
     },
-    "restaurant_complaint": {
+    {
+        "slug": "restaurant_complaint",
         "code": "4fd6e4ece750840569000019",
         "name": "Restaurant Complaint"
     },
-    "rodent_baiting_rat_complaint": {
+    {
+        "slug": "rodent_baiting_rat_complaint",
         "code": "4fd3b9bce750846c5300004a",
         "name": "Rodent Baiting / Rat Complaint"
     },
-    "tree_debris": {
+    {
+        "slug": "tree_debris",
         "code": "4fd3bbf8e750846c53000069",
         "name": "Tree Debris"
     },
-    "abandoned_vehicle": {
+    {
+        "slug": "abandoned_vehicle",
         "code": "4ffa4c69601827691b000018",
         "name": "Abandoned Vehicle"
     },
-    "street_light_1_out": {
+    {
+        "slug": "street_light_1_out",
         "code": "4ffa9f2d6018277d400000c8",
         "name": "Street Light 1 / Out"
     },
-    "pavement_cavein_survey": {
+    {
+        "slug": "pavement_cavein_survey",
         "code": "4ffa971e6018277d4000000b",
         "name": "Pavement Cave-In Survey"
     },
-    "alley_light_out": {
+    {
+        "slug": "alley_light_out",
         "code": "4ffa9cad6018277d4000007b",
         "name": "Alley Light Out"
     },
-    "building_violation": {
+    {
+        "slug": "building_violation",
         "code": "4fd3bd72e750846c530000cd",
         "name": "Building Violation"
     },
-    "traffic_signal_out": {
+    {
+        "slug": "traffic_signal_out",
         "code": "4ffa9db16018277d400000a2",
         "name": "Traffic Signal Out"
     },
-    "street_cut_complaint": {
+    {
+        "slug": "street_cut_complaint",
         "code": "4ffa995a6018277d4000003c",
         "name": "Street Cut Complaints"
     },
-    "sanitation_code_violation": {
+    {
+        "slug": "sanitation_code_violation",
         "code": "4fd3b750e750846c5300001d",
         "name": "Sanitation Code Violation"
     },
-    "pothole_in_street": {
+    {
+        "slug": "pothole_in_street",
         "code": "4fd3b656e750846c53000004",
         "name": "Pothole in Street"
     },
-    "street_lights_all_out": {
+    {
+        "slug": "street_lights_all_out",
         "code": "4fd3bd3de750846c530000b9",
         "name": "Street Lights All / Out"
     }
+];
+
+window.stSlugs = _.pluck(serviceTypesJSON, 'slug');
+
+window.lookupSlug = function(slug) {
+    return _.find(serviceTypesJSON,function(obj) {return obj.slug == slug;});
 };
 
 window.buildWardPaths = function() {
