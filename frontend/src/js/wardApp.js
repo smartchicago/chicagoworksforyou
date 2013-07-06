60db82e0f24071d1380e7c9e724e4c2f10d3a3ed
Sandor Weisz <santheo@mac.com>
2013-07-06 18:19:04 -0500

    Moved Ward detail Angular code to its own file.

--- frontend/src/js/wardApp.js
+++ frontend/src/js/wardApp.js
@@ -1,3 +1,5 @@
+// JQUERY
+
 $(function () {
     var centroid = WARDS[wardNum].centroid;
     var wardCenter = [centroid[1], centroid[0]];
@@ -96,3 +98,100 @@ $(function () {
     });
 });
 
+// ANGULAR
+
+var wardApp = angular.module('wardApp', []);
+
+wardApp.config(function($routeProvider) {
+    $routeProvider.
+        when('/:serviceSlug', {
+            controller: "wardCtrl",
+            templateUrl: "/views/ward_charts.html"
+        }).
+        when('/:serviceSlug/:date', {
+            controller: "wardCtrl",
+            templateUrl: "/views/ward_charts.html"
+        }).
+        otherwise({
+            redirectTo: '/graffiti_removal'
+        });
+});
+
+// WARD DETAIL
+
+wardApp.controller("serviceListCtrl", function ($scope, $http, $location) {
+    $http.get('/data/services.json').success(function(data) {
+        $scope.services = data;
+    });
+    $scope.orderProp = 'name';
+
+    $scope.isActive = function(slug) {
+        var currServiceSlug = $location.path().substr(1);
+        return slug == currServiceSlug;
+    };
+});
+
+wardApp.controller("wardCtrl", function ($scope, $location, $routeParams) {
+    var serviceType = window.lookupSlug($routeParams.serviceSlug);
+    var serviceCode = serviceType.code;
+
+    // CHARTS WEEKNAV
+
+    $('.this-week a').click(function(evt) {
+        evt.preventDefault();
+    });
+
+    $('.next-week a').click(function(evt) {
+        evt.preventDefault();
+        currWeekEnd.add('week',1);
+        $.getJSON(
+            window.apiDomain + 'wards/' + wardNum + '/counts.json?count=7&service_code=' + serviceCode + '&end_date=' + currWeekEnd.format(dateFormat) + '&callback=?',
+            function(response) {redrawChart(response);}
+        );
+    });
+
+    $('.prev-week a').click(function(evt) {
+        evt.preventDefault();
+        currWeekEnd.subtract('week',1);
+        $.getJSON(
+            window.apiDomain + 'wards/' + wardNum + '/counts.json?count=7&service_code=' + serviceCode + '&end_date=' + currWeekEnd.format(dateFormat) + '&callback=?',
+            function(response) {redrawChart(response);}
+        );
+    });
+
+    // CHART
+
+    $.getJSON(
+        window.apiDomain + 'wards/' + wardNum + '/counts.json?count=7&service_code=' + serviceCode + '&end_date=' + currWeekEnd.format(dateFormat) + '&callback=?',
+        function(response) {redrawChart(response);}
+    );
+
+    function redrawChart(response) {
+        var categories = [];
+        var counts = [];
+        for (var d in response) {
+            categories.push(moment(d).format("MMM DD"));
+            counts.push(response[d]);
+        }
+        countsChart.series[0].setData(counts);
+        countsChart.xAxis[0].setCategories(categories);
+        var currWeek = weekDuration.beforeMoment(currWeekEnd,true);
+        $('.this-week a').text(currWeek.format({implicitYear: false}));
+    }
+
+    var countsChart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'counts-chart'
+        },
+        series: [{
+            name: "Ward " + wardNum
+        },{
+            name: "City average",
+            data: [5, 6, 7, 8, 4, 3, 9],
+            type: 'line',
+            dashStyle: 'longdash'
+        }]
+    });
+
+    $scope.wardNum = wardNum;
+});
