c10962a9428d42dfacbd20150b4cc2c01d859c27
Chris Gansen <cgansen@gmail.com>
2013-07-27 17:34:29 -0500

    adding task to capture snapshot of the database

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server "cwfy-api.smartchicagoapps.org", :app, :db, primary: true
 set :branch, 'master'
 set :deploy_to, "/var/www/cwfy/production"
-set :supervisor_group, "production"
+set :stage, "production"
+set :database, "cwfy-production"
