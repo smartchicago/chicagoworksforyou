359080c71dc2cb83a3ad6e3b5f957907cd4ddad2
Sandor Weisz <santheo@mac.com>
2013-07-30 18:42:31 -0500

    Moved most JS code out of the jQuery block.

--- frontend/src/js/wardApp.js
+++ frontend/src/js/wardApp.js
@@ -1,31 +1,10 @@
+var centroids = [null,[-87.683231,41.914304],[-87.648803,41.867456],[-87.62964,41.813566],[-87.603125,41.816155],[-87.586705,41.776022],[-87.618591,41.748371],[-87.560683,41.738814],[-87.588308,41.735931],[-87.610453,41.678697],[-87.55646,41.687401],[-87.653323,41.829887],[-87.69005,41.827565],[-87.733226,41.769769],[-87.711642,41.807895],[-87.684336,41.779294],[-87.668058,41.791246],[-87.654714,41.761173],[-87.697921,41.749512],[-87.68919,41.700988],[-87.62324,41.78518],[-87.645912,41.73037],[-87.723363,41.838713],[-87.762182,41.792351],[-87.723381,41.86401],[-87.662184,41.855632],[-87.703834,41.906883],[-87.676908,41.892697],[-87.724581,41.879511],[-87.770902,41.897949],[-87.740537,41.930753],[-87.743533,41.928236],[-87.669543,41.92199],[-87.705979,41.955858],[-87.643502,41.687812],[-87.709059,41.931693],[-87.815517,41.939613],[-87.751139,41.905722],[-87.769601,41.952115],[-87.729676,41.978135],[-87.686528,41.985198],[-87.864869,41.984042],[-87.625883,41.890473],[-87.641061,41.919772],[-87.652198,41.942085],[-87.763801,41.975583],[-87.65149,41.960896],[-87.68137,41.959399],[-87.658782,41.982538],[-87.671545,42.011679],[-87.697288,42.002965]];
+var wardCentroid = centroids[wardNum];
+var wardCenter = [wardCentroid[1], wardCentroid[0]];
+
 // JQUERY
 
 $(function () {
-    var centroids = [null,[-87.683231,41.914304],[-87.648803,41.867456],[-87.62964,41.813566],[-87.603125,41.816155],[-87.586705,41.776022],[-87.618591,41.748371],[-87.560683,41.738814],[-87.588308,41.735931],[-87.610453,41.678697],[-87.55646,41.687401],[-87.653323,41.829887],[-87.69005,41.827565],[-87.733226,41.769769],[-87.711642,41.807895],[-87.684336,41.779294],[-87.668058,41.791246],[-87.654714,41.761173],[-87.697921,41.749512],[-87.68919,41.700988],[-87.62324,41.78518],[-87.645912,41.73037],[-87.723363,41.838713],[-87.762182,41.792351],[-87.723381,41.86401],[-87.662184,41.855632],[-87.703834,41.906883],[-87.676908,41.892697],[-87.724581,41.879511],[-87.770902,41.897949],[-87.740537,41.930753],[-87.743533,41.928236],[-87.669543,41.92199],[-87.705979,41.955858],[-87.643502,41.687812],[-87.709059,41.931693],[-87.815517,41.939613],[-87.751139,41.905722],[-87.769601,41.952115],[-87.729676,41.978135],[-87.686528,41.985198],[-87.864869,41.984042],[-87.625883,41.890473],[-87.641061,41.919772],[-87.652198,41.942085],[-87.763801,41.975583],[-87.65149,41.960896],[-87.68137,41.959399],[-87.658782,41.982538],[-87.671545,42.011679],[-87.697288,42.002965]];
-    var wardCentroid = centroids[wardNum];
-    var wardCenter = [wardCentroid[1], wardCentroid[0]];
-
-    // WARD MAP
-
-    var map = L.map('map', {scrollWheelZoom: false}).setView(wardCenter, 13);
-    L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        key: '302C8A713FF3456987B21FAAE639A13B',
-        maxZoom: 18,
-        styleId: 82946
-    }).addTo(map);
-    map.zoomControl.setPosition('bottomleft');
-    var polygon = L.polygon(wardPaths[wardNum - 1],
-        {
-            opacity: 1,
-            weight: 2,
-            dashArray: '3',
-            color: '#182A35',
-            fillOpacity: 0.7,
-            fillColor: '#4888AF'
-        }
-    ).addTo(map);
-
     // MAKE FILTER STICK
 
     $(".filter").affix({
@@ -60,6 +39,29 @@ wardApp.factory('Data', function ($location, $route, $routeParams) {
     var data = {
         wardNum: window.wardNum
     };
+
+    // WARD MAP
+
+    if (!window.chicagoMap) {
+        window.chicagoMap = L.map('map', {scrollWheelZoom: false}).setView(wardCenter, 13);
+        L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+            key: '302C8A713FF3456987B21FAAE639A13B',
+            maxZoom: 18,
+            styleId: 82946
+        }).addTo(window.chicagoMap);
+        window.chicagoMap.zoomControl.setPosition('bottomleft');
+        var polygon = L.polygon(wardPaths[wardNum - 1],
+            {
+                opacity: 1,
+                weight: 2,
+                dashArray: '3',
+                color: '#182A35',
+                fillOpacity: 0.7,
+                fillColor: '#4888AF'
+            }
+        ).addTo(window.chicagoMap);
+    }
 
     data.setDate = function(date) {
         data.date = date.format(dateFormat);
@@ -100,7 +102,7 @@ wardApp.controller("wardOverviewCtrl", function ($scope, Data, $http, $location,
     $scope.data = Data;
 });
 
-wardApp.controller("wardCtrl", function ($scope, Data, $http, $location, $routeParams) {
+wardApp.controller("wardCtrl", function ($scope, Data, $http, $location, $routeParams, $timeout) {
     Data.setDate(parseDate($routeParams.date, window.yesterday, $location));
 
     var serviceObj = {};
@@ -196,6 +198,8 @@ wardApp.controller("wardCtrl", function ($scope, Data, $http, $location, $routeP
                 Data.wardRank = window.getOrdinal(position + 1);
                 Data.wardTime = Math.round(sorted[position].Time * 100) / 100;
             }
+
+
 
             var ttcChart = new Highcharts.Chart({
                 chart: {
