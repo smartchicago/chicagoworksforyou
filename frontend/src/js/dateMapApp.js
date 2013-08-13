c7a428c205dce77684872e2fbca714c2fd9ba1dc
Sandor Weisz <santheo@mac.com>
2013-08-13 16:25:04 -0500

    Don't allow people to zoom in. Only zoom out.

--- frontend/src/js/dateMapApp.js
+++ frontend/src/js/dateMapApp.js
@@ -28,7 +28,7 @@ dateMapApp.factory('Data', function () {
         L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
             key: '302C8A713FF3456987B21FAAE639A13B',
-            maxZoom: 18,
+            maxZoom: 11,
             styleId: 82946
         }).addTo(window.chicagoMap);
         window.chicagoMap.zoomControl.setPosition('bottomright');
