490f38a96f03ac1f285a2c17b051dd16dfa6bcf8
cgansen <cgansen@gmail.com>
2013-08-13 17:32:47 -0500

    move API helper functions to a separate file

---
+++ api/helpers.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"strings"
+	"net/http"
+	"encoding/json"
+	"log"
+	"time"
+	"fmt"
+)
+
+func WrapJson(unwrapped []byte, callback []string) (jsn []byte) {
+	jsn = unwrapped
+	if len(callback) > 0 {
+		wrapped := strings.Join([]string{callback[0], "(", string(jsn), ");"}, "")
+		jsn = []byte(wrapped)
+	}
+
+	return
+}
+
+func endpoint(f ApiEndpoint) http.HandlerFunc {
+	// define an endpoint and setup consistent logging
+	
+	return func(w http.ResponseWriter, req *http.Request) {
+		w = setHeaders(w)
+		params := req.URL.Query()
+
+		log.Printf("[cwfy %s] %s %s%s %+v", version, req.RemoteAddr, req.Host, req.RequestURI, params)
+
+		t := time.Now()
+		response, err := f(params, req)
+
+		if err != nil {
+			log.Printf(err.Error())
+			http.Error(w, err.Msg, err.Code)
+		}
+
+		w.Write(WrapJson(response, params["callback"]))
+		diff := time.Now()
+		log.Printf("[cwfy %s] %s %s%s completed in %v", version, req.RemoteAddr, req.Host, req.RequestURI, diff.Sub(t))
+	}
+}
+
+func setHeaders(w http.ResponseWriter) http.ResponseWriter {
+	// set HTTP headers on the response object
+	// TODO: add cache control headers
+
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
+	w.Header().Set("Server", fmt.Sprintf("ChicagoWorksForYou.com/%s", version))
+	return w
+}
+
+func dumpJson(in interface{}) []byte {
+	out, err := json.MarshalIndent(in, "", "  ")
+	if err != nil {
+		log.Printf("error marshalling to json: %s", err)
+	}
+	return out
+}
--- api/server.go
+++ api/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -14,7 +13,6 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -77,54 +75,6 @@ type ApiError struct {
 
 func (e *ApiError) Error() string {
 	return fmt.Sprintf("api error %d: %s", e.Code, e.Msg)
-}
-
-func endpoint(f ApiEndpoint) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		w = setHeaders(w)
-		params := req.URL.Query()
-
-		log.Printf("[cwfy %s] %s %s%s %+v", version, req.RemoteAddr, req.Host, req.RequestURI, params)
-
-		t := time.Now()
-		response, err := f(params, req)
-
-		if err != nil {
-			log.Printf(err.Error())
-			http.Error(w, err.Msg, err.Code)
-		}
-
-		w.Write(WrapJson(response, params["callback"]))
-		diff := time.Now()
-		log.Printf("[cwfy %s] %s %s%s completed in %v", version, req.RemoteAddr, req.Host, req.RequestURI, diff.Sub(t))
-	}
-}
-
-func setHeaders(w http.ResponseWriter) http.ResponseWriter {
-	// set HTTP headers on the response object
-	// TODO: add cache control headers
-
-	w.Header().Set("Content-type", "application/json; charset=utf-8")
-	w.Header().Set("Server", fmt.Sprintf("ChicagoWorksForYou.com/%s", version))
-	return w
-}
-
-func dumpJson(in interface{}) []byte {
-	out, err := json.MarshalIndent(in, "", "  ")
-	if err != nil {
-		log.Printf("error marshalling to json: %s", err)
-	}
-	return out
-}
-
-func WrapJson(unwrapped []byte, callback []string) (jsn []byte) {
-	jsn = unwrapped
-	if len(callback) > 0 {
-		wrapped := strings.Join([]string{callback[0], "(", string(jsn), ");"}, "")
-		jsn = []byte(wrapped)
-	}
-
-	return
 }
 
 // func HealthCheckHandler(response http.ResponseWriter, request *http.Request) {
