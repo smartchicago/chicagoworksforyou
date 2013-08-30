737fbff7c34bf08fbda9775323cb6f1eabfb7f50
Chris Gansen <cgansen@gmail.com>
2013-08-30 13:00:14 -0500

    rm unused city averages code

--- api/ward_counts_handler.go
+++ api/ward_counts_handler.go
@@ -12,7 +12,7 @@ import (
 )
 
 func WardCountsHandler(params url.Values, request *http.Request) ([]byte, *ApiError) {
-	// for a given ward, return the number of service requests opened
+	// for a given ward, return the number of service requests opened and closed
 	// grouped by day, then by service request type
 	//
 	// Parameters:
@@ -24,34 +24,39 @@ func WardCountsHandler(params url.Values, request *http.Request) ([]byte, *ApiEr
 	//
 	// Sample API output
 	//
-	// Note that the end date is June 12, and the results include the end_date. Days with no service requests will report "0"
+	// Note that the end date is August 30, and the results include the end_date. Days with no service requests will report "0"
 	//
-	// $ curl "http://localhost:5000/wards/10/counts.json?count=7&end_date=2013-08-30"
+        // $ curl "http://localhost:5000/wards/10/counts.json?count=7&end_date=2013-08-30"
         // {
-        // "2013-08-24": {
-        //   "Opened": 0,
-        //   "Closed": 0,
-        //   "CityTotal": 0,
-        //   "CityAverage": 0
-        // },
-        // "2013-08-25": {
-        //   "Opened": 0,
-        //   "Closed": 0,
-        //   "CityTotal": 0,
-        //   "CityAverage": 0
-        // },
-        // "2013-08-26": {
-        //   "Opened": 7,
-        //   "Closed": 4,
-        //   "CityTotal": 0,
-        //   "CityAverage": 0
-        // },
-        // "2013-08-27": {
-        //   "Opened": 20,
-        //   "Closed": 37,
-        //   "CityTotal": 0,
-        //   "CityAverage": 0
-        // },
+        //   "2013-08-24": {
+        //     "Opened": 0,
+        //     "Closed": 0
+        //   },
+        //   "2013-08-25": {
+        //     "Opened": 0,
+        //     "Closed": 0
+        //   },
+        //   "2013-08-26": {
+        //     "Opened": 7,
+        //     "Closed": 4
+        //   },
+        //   "2013-08-27": {
+        //     "Opened": 20,
+        //     "Closed": 37
+        //   },
+        //   "2013-08-28": {
+        //     "Opened": 18,
+        //     "Closed": 34
+        //   },
+        //   "2013-08-29": {
+        //     "Opened": 7,
+        //     "Closed": 6
+        //   },
+        //   "2013-08-30": {
+        //     "Opened": 0,
+        //     "Closed": 0
+        //   }
+        // }
 
 	vars := mux.Vars(request)
 	ward_id := vars["id"]
@@ -96,8 +101,6 @@ func WardCountsHandler(params url.Values, request *http.Request) ([]byte, *ApiEr
 	type WardCount struct {
 		Opened      int
 		Closed      int
-		CityTotal   int
-		CityAverage float32
 	}
 
 	counts := make(map[string]WardCount)
@@ -110,35 +113,6 @@ func WardCountsHandler(params url.Values, request *http.Request) ([]byte, *ApiEr
 		}
 
 		counts[rd.Format("2006-01-02")] = wc
-	}
-
-	// calculate the citywide average for each day
-	rows, err = api.Db.Query(`SELECT COUNT(*), DATE(requested_datetime) AS requested_date 
-		FROM service_requests 
-		WHERE duplicate IS NULL 
-			AND service_code = $1 
-			AND requested_datetime >= $2::date 
-			AND requested_datetime <= $3::date
-		GROUP BY DATE(requested_datetime) 
-		ORDER BY requested_date;`,
-		service_code, start, end)
-
-	if err != nil {
-		log.Fatal("error fetching data for WardCountsHandler", err)
-	}
-
-	for rows.Next() {
-		var rd time.Time
-		var city_total int
-		if err := rows.Scan(&city_total, &rd); err != nil {
-			log.Print("error reading row of ward count", err)
-		}
-
-		k := rd.Format("2006-01-02")
-		tmp := counts[k]
-		tmp.CityTotal = city_total
-		tmp.CityAverage = float32(city_total) / 50.0
-		counts[k] = tmp
 	}
 
 	resp := make(map[string]WardCount)
