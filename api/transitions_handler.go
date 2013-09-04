f92f551a8be23d29c865bbed00eb7a172d536bc9
cgansen <cgansen@gmail.com>
2013-09-04 10:23:22 -0500

    removing bits from returning all transition areas

--- api/transitions_handler.go
+++ api/transitions_handler.go
@@ -22,23 +22,18 @@ func TransitionsHandler(params url.Values, request *http.Request) ([]byte, *ApiE
 		Incoming, Outgoing []Transition
 	}
 
-	var transitions Changes
-	// [ { 'id': 123, 'Ward2013': 42, 'Ward2015': 35, 'Boundary': <GeoJSON> },  ] }
+	var c Changes
 
-	w, err := strconv.Atoi(ward)	
+	w, err := strconv.Atoi(ward)
 	if err != nil || w < 1 || w > 50 {
 		return nil, &ApiError{Code: 400, Msg: "invalid ward"}
 	}
 
-	query := "SELECT ward_2013,ward_2015,id, ST_AsGeoJSON(boundary, 5, 2) FROM transition_areas %s ORDER BY ward_2013;"
-
-	if w != 0 {
-		query = fmt.Sprintf(query, fmt.Sprintf("WHERE ward_2013 = %d OR ward_2015 = %d", w, w))
-	} else {
-		query = fmt.Sprintf(query, "")
-	}
-
-	rows, err := api.Db.Query(query)
+	rows, err := api.Db.Query(`SELECT ward_2013,ward_2015,id, ST_AsGeoJSON(boundary, 5, 2) 
+		FROM transition_areas 
+		WHERE ward_2013 = $1 
+			OR ward_2015 = $2 
+		ORDER BY ward_2013;`, w, w)
 	if err != nil {
 		log.Printf("error fetching transition areas: %s", err)
 	}
@@ -48,13 +43,13 @@ func TransitionsHandler(params url.Values, request *http.Request) ([]byte, *ApiE
 		if err := rows.Scan(&t.Ward2013, &t.Ward2015, &t.Id, &t.Boundary); err != nil {
 			log.Printf("error loading transition area result: %s", err)
 		}
-		
+
 		if t.Ward2013 == w {
-			transitions.Outgoing = append(transitions.Outgoing, t)
+			c.Outgoing = append(c.Outgoing, t)
 		} else {
-			transitions.Incoming = append(transitions.Incoming, t)
+			c.Incoming = append(c.Incoming, t)
 		}
 	}
 
-	return dumpJson(transitions), nil
+	return dumpJson(c), nil
 }
